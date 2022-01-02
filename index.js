10cf9e10acda3e7a12cc3ee610c30bb6b631aec9
John Mook <jedwardmook@yahoo.com>
2022-01-01 18:25:34 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat() {cats.push("Ralph")}
+
+function destructivelyPrependCat() {cats.unshift("Bob")}
+
+function destructivelyRemoveLastCat() {cats.pop()}
+
+function destructivelyRemoveFirstCat() {cats.shift()}
+ 
+const newCat = ["Broom"];
+function appendCat(name) {
+    let newCat = [...cats, "Broom"]
+    return newCat
+}
+
+const aCat = ["Arnold"]
+function prependCat(name) {
+    let aCat = ["Arnold", ...cats]
+    return aCat
+}
+
+const lastCat = ["Milo", "Otis"]
+function removeLastCat(name) {
+    lastCat.slice(name);
+    return lastCat
+}
+
+const firstCat = ["Otis", "Garfield"]
+function removeFirstCat(name) {
+    firstCat.slice(name)
+    return firstCat
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
